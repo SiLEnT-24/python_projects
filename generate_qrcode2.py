f4631b7159e83fcce1df92aa802a9bd0cf0a12b3
SilEnT-24 <ak4689769@gmail.com>
2026-07-13 22:14:41 +0530

    Fix QR code image colors to use black foreground and white background

--- generate_qrcode2.py
+++ generate_qrcode2.py
@@ -8,6 +8,6 @@ qr=qrcode.QRCode(version=1, error_correction=qrcode.constants.ERROR_CORRECT_H,
 qr.add_data("https://search.brave.com/search?q=qr+code+module+in+python&summary=1&conversation=095123df7fd4044f7b358fa3277a0f27292f")
 qr.make(fit=True)
 
-img=qr.make_image(fill_color="blue", back_color="black")
+img=qr.make_image(fill_color="black", back_color="white")
 
 img.save("qrcode_brave.png")
