973b2d4cf963b00681ae07ab8040f60b364affc2
SilEnT-24 <ak4689769@gmail.com>
2026-07-14 13:07:38 +0530

    Update email validation comments and add user input prompt

--- CheckValidateE_Mail/gmail1.py
+++ CheckValidateE_Mail/gmail1.py
@@ -1,8 +1,11 @@
-# a-z
+# a-z  ak028474@gmail.com
 # 0-9
 # . _ time 1
 # @ time 1
 # . 2,3 positions
 
 import re
+email_condition="^[a-z]+[\._]?[a-z 0-9]+[@]\a+[.]\a{2,3}$"
+user_email=input('enter your email :')
+
 
