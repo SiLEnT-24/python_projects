ba6eac46a0725ac9774f736faf18bd22bfcf15b3
SilEnT-24 <ak4689769@gmail.com>
2026-07-14 21:09:32 +0530

    Fix syntax errors in print statements and conditional checks in Student Manager app

--- StudentResultManager/StudentManager.py
+++ StudentResultManager/StudentManager.py
@@ -1,50 +1,42 @@
 student = {}
 
 while True:
-    print{"\n------STUDENT MANAGER APP------"}
-    print{"1. Add Student"}
-    print{"2. View Students"}
-    print{"3. Check Result"}
-    print{"4. Exit"}
+    print("\n------STUDENT MANAGER APP------")
+    print("1. Add Student")
+    print("2. View Students")
+    print("3. Check Result")
+    print("4. Exit")
 
-choice = input("Enter your choice : ")
+    choice = input("Enter your choice : ")
 
-# Add Student
+    if choice == "1":
+        name = input("Enter student name: ")
+        marks = int(input("Enter marks: "))
+        student[name] = marks
+        print(f"{name} Successfully Added!")
 
-if choice == "1":
-    name = input("Enter student name: ")
-    marks = int(input("Enter marks: "))
-    student[name] = marks
-    print(f"{name} Successfully Added!")
-
-
-#view students
-elif choice == "2":
-    if not student:
-        print("No student found!")
-    else:
-        for name, marks in student.items():
-            print(name,":", marks)
-
-#Check result    
-
-elif choice = "3":
-    name = input("Enter student name:")
-
-    if name in student:
-        mark = student[name]
-
-        if marks >= 40:
-            print("PASS")
-
+    elif choice == "2":
+        if not student:
+            print("No student found!")
         else:
-            print("FAIL") 
+            for name, marks in student.items():
+                print(name, ":", marks)
 
+    elif choice == "3":
+        name = input("Enter student name:")
+
+        if name in student:
+            mark = student[name]
+
+            if mark >= 40:
+                print("PASS")
+            else:
+                print("FAIL")
+        else:
+            print("Student not found!")
+
+    elif choice == "4":
+        print("Exiting...")
+        break
     else:
-        print("Student not found!")
-
-elif Choice == "4":
-    print("Exiting...")
-    break
-else:
-    print("invalid input")
+        print("invalid input")
