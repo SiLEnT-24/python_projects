7b51117d7b8e8c7c3f208a364972f8497ad9390a
SilEnT-24 <ak4689769@gmail.com>
2026-07-15 17:55:27 +0530

    Refactor password loading logic and enhance password generation function

--- PasswordManager/Password_Manager.py
+++ PasswordManager/Password_Manager.py
@@ -4,3 +4,23 @@ import string
 passwords = {}
 
 # load exiting password file
+
+try:
+    with open ("Password.text", "r") as file:
+        for line in file :
+            website,pwd = line.strip().split(":")
+            passwords[website] = pwd
+
+except:
+    pass
+
+def generate_password():
+      chars = string.ascii_letters+string.digits+"!@#$%&"
+      password ="".join(random.choice(chars) for _ in range(8))
+
+while True:
+    print("\n-----PASSWORD MANAGER-----")
+    print("1. Save Password")
+    print("2. View Passwords")
+    print("3. Generate Password")
+    print("4. Exit")
