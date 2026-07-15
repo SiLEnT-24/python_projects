53cacc464923f44ba82569f74a26a67f2166e085
SilEnT-24 <ak4689769@gmail.com>
2026-07-15 18:06:28 +0530

    Fix indentation and structure in Password Manager menu logic

--- PasswordManager/Password_Manager.py
+++ PasswordManager/Password_Manager.py
@@ -24,3 +24,34 @@ while True:
     print("2. View Passwords")
     print("3. Generate Password")
     print("4. Exit")
+    
+    choice = input("Enter your choice:")
+
+    if choice == "1":
+        site = input("Enter website:")
+        pwd = input("Enter password:")
+
+        passwords[site] = pwd
+
+        with open("Passwords.text", "a")as file:
+            file.write(f"{site}:{pwd}\n")
+
+        print("Saved!")
+
+    elif choice == "2":
+        if not passwords:
+            print("No data")
+
+        else:
+            for site, pwd in passwords.items():
+                 print(site,":",pwd)
+
+        elif choice == "3":
+            print("Generated Password", generate_password())
+        
+        elif choice == "4":
+            print("exit...")
+            break
+
+        else:
+            print("Invalid input")
