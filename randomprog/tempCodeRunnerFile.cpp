bdcc1215d0192f6d5a0afe43518c5262cb94dec1
SilEnT-24 <ak4689769@gmail.com>
2026-09-22 11:52:34 +0530

    Add initial files: Jupyter notebook, HTML table, and C++ code for basic arithmetic operations
    - Created a Jupyter notebook (`firstcode.ipynb`) with a simple print statement.
    - Added an HTML file (`table.html`) for a weekly schedule with structured table data.
    - Introduced a C++ file (`tempCodeRunnerFile.cpp`) containing code for basic arithmetic operations (sum, difference, product, quotient, remainder).

---
+++ randomprog/tempCodeRunnerFile.cpp
@@ -0,0 +1,6 @@
+   cin>>a>>b;
+    cout<<"Sum: "<<a+b<<endl;
+    cout<<"Difference: "<<a-b<<endl;
+    cout<<"Product: "<<a*b<<endl;
+    cout<<"Quotient: "<<a/b<<endl;
+    cout<<"Remainder: "<<a%b<<endl;
